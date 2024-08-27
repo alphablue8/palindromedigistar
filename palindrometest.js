778f5167efa9c4445925e7f2de210063fefff724
Anggoro Maliky <79727890+alphablue8@users.noreply.github.com>
2024-08-27 18:23:45 +0700

    npm palindrome test 1

---
+++ palindrometest.js
@@ -0,0 +1,6 @@
+const isPalindrome = require('PalindromeDigistar')
+
+console.log(isPalindrome("Digistar")); // false
+console.log(isPalindrome("rotator")); // true
+console.log(isPalindrome("racecar")); // true
+console.log(isPalindrome("hello")); // false
